5fd78c1a2ff2f9960d3380451dbaf5e2b2e47eed
iandresw <rbejarano@unah.hn>
2022-11-05 02:10:54 -0600

    first commit

--- src/com/AnsdresBejarano/HolaMundo/operadores.java
+++ src/com/AnsdresBejarano/HolaMundo/operadores.java
@@ -10,6 +10,9 @@ public class operadores {
 		int a = 2;
 		int b = 5;
 		int c = 0;
+		System.out.println("\n");	
+		System.out.println("*************OPERACIONES BASICAS*********");
+		System.out.println("\n");	
 		
 		//Suma
 		
@@ -30,6 +33,124 @@ public class operadores {
 		//Modulo
 		c = a % b;
 		System.out.println("El resultado del modulo  de  "+ a + " y "+ b + " es igual a: " + c);
+		
+		
+		//Operaciones de asignaciones con operacion
+		// += -= *= /=
+		System.out.println("\n");	
+		System.out.println("*************OPERACIONES DE ASIGNACION CON OPERACION*********");
+		System.out.println("\n");	
+		
+		c = 2;
+		c += a; // c =c + a
+		System.out.println("c+= - >>> " + c);
+		
+		c = 2;
+		c -= a;		// c= c - a
+		System.out.println("c-= - >>> " + c );		
+		c = 2;
+		c *= a;		// c= c - a
+		System.out.println("c*= - >>> " + c);
+		
+		c = 2;
+		c /= a;		// c= c - a
+		System.out.println("c/= - >>> " + c);
+		
+		c = 2;
+		c %= a;		// c= c - a
+		System.out.println("c%= - >>> " + c);
+		
+		
+		System.out.println("\n");	
+		System.out.println("*************OPERADOR INCREMENTO (++) Y DECREMENTO (--) PREFIJO*********");
+		System.out.println("\n");	
+		int i = 3;
+		++i;
+		System.out.println("Incremento prefijo ++i " + i);
+		--i;
+		System.out.println("Decremento prefijo --i " + i);
+		System.out.println("Impresion Decremento prefijo --i " + --i);
+		System.out.println(" Decremento prefijo despues de impresion   " + i);
+		
+		System.out.println("\n");	
+		System.out.println("*************OPERADOR INCREMENTO (++) Y DECREMENTO (--) POSFIJO*********");
+		System.out.println("\n");	
+		i = 3;
+		i++;
+		System.out.println("Incremento prefijo i++ " + i);
+		i--;
+		System.out.println("Decremento prefijo i-- " + i);
+		System.out.println("Impresion Decremento posfijo --i " + i--);
+		System.out.println(" Decremento posfijo despues de impresion   " + i);
+		
+		System.out.println("\n");	
+		System.out.println("*************OPERADORES DE EQUIDAD*********");
+		System.out.println("\n");
+		
+		a = 5;
+		b = 4;
+		
+		System.out.println("a es igual a b (a==b)         "+ (a==b));
+		System.out.println("a es distinto a b (a!=b))     "+ (a!=b));
+		
+		System.out.println("a es mayor a b (a > b)        "+ (a > b));
+		System.out.println("a es menor a b (a < b)        "+ (a < b));
+		
+		System.out.println("a es mayor igual  a b (a>=b)  "+ (a>=b));
+		System.out.println("a es menor igual a b (a<=b)   "+ (a<=b));
+		
+		
+		
+		System.out.println("\n");	
+		System.out.println("*************OPERADORES DE ASOCIACION AND &&*********");
+		System.out.println("\n");
+		
+		boolean q = false;
+		boolean w = true;
+		
+		System.out.println("w && q ---->    " + (q && w));
+		System.out.println("w || q ---->    " + (q || w));
+		System.out.println("w !w q ---->    " + (!w));
+		
+		System.out.println("\n");	
+		System.out.println("*************OPERADORES DE CONJUNCION O   &&*********");
+		System.out.println("\n");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
